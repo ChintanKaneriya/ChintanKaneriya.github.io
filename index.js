617e79b8dd24862d8120550a4779f9ff5617f121
Chintan Kaneriya <chintankaneriya5@gmail.com>
2025-07-17 21:34:10 +0530

    React starter

---
+++ index.js
@@ -0,0 +1,14 @@
+function MyButton () {
+    return (
+        <button>Click here</button>
+    );
+}
+
+export default function MyApp () {
+    return (
+        <div class="main-container">
+            <h1>Enter the dragon</h1>
+            <MyButton />
+        </div>
+    );
+}
